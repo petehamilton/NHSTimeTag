8ef07033847e05a9f9b37658c108c6f4ababd5eb
Peter Hamilton <peter@inspiredpixel.net>
2011-06-17 23:37:17 +0100

    Removed unused function

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -2,11 +2,4 @@ class ClinicsController < ApplicationController
   def index
     
   end
-  def status
-    respond_to do |format|
-      format.json{
-        render :json => @clinic.to_json
-      }
-    end
-  end
 end
