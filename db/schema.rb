849c4b2957e44442aa2f3cf6c1da7f10de36523f
Peter Hamilton <peter@inspiredpixel.net>
2011-06-17 23:36:52 +0100

    Few extra updates to change consultant to doctor

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(:version => 20110617143946) do
   create_table "appointments", :force => true do |t|
     t.datetime "datetime"
     t.integer  "patient_id"
-    t.integer  "clinic_id"
+    t.integer  "doctor_id"
     t.boolean  "attending",  :default => true
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -28,8 +28,9 @@ ActiveRecord::Schema.define(:version => 20110617143946) do
     t.datetime "updated_at"
   end
 
-  create_table "consultants", :force => true do |t|
+  create_table "doctors", :force => true do |t|
     t.string   "title"
+    t.string   "first_name"
     t.string   "last_name"
     t.integer  "clinic_id"
     t.datetime "created_at"
