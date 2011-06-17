78346b827ecb3fa64ffa7e72271bed69905848ca
Peter Hamilton <peter@inspiredpixel.net>
2011-06-18 00:18:40 +0100

    Made a simple version of the consultant view, added function to doctor to get current status, fixed clinic so it has many doctors, not just one

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -4,4 +4,15 @@ class Doctor < ActiveRecord::Base
   def name
     return "#{self.first_name} #{self.last_name}"
   end
+  
+  def get_status
+    case self.current_delay
+      when 0..60
+        return "Running on time"
+      when 60..120
+        return "Delayed"
+      else
+        return "Severely Delayed"
+    end
+  end
 end
