376c3b3a6d2a628666703656caea69ab54632575
Peter Hamilton <peter@inspiredpixel.net>
2011-06-17 15:12:31 +0100

    Added skeleton rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NHSTimeTag::Application.config.secret_token = '04fc85abe24689a8e1e5464a852d2375470ae7a1ef94f6592eb0b4c18c74c3f712296b7b943199699bffd9361dd2b9863079fce4301e6720cd8da5ca74165f60'
