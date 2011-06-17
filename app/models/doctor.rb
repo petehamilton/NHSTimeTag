ce6b8c0e26e5632af18c2862344bb9e48fe6603f
Peter Hamilton <peter@inspiredpixel.net>
2011-06-17 23:37:31 +0100

    Added name function to doctor and patient models

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,7 @@
 class Doctor < ActiveRecord::Base
   belongs_to :clinic
+  
+  def name
+    return "#{self.first_name} #{self.last_name}"
+  end
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,7 @@
 class Patient < ActiveRecord::Base
   has_many :appointments
+  
+  def name
+    return "#{self.first_name} #{self.last_name}"
+  end
 end
