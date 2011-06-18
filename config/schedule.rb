52ff831ce082bed61261f10908b0fb5a8843a43c
Peter Hamilton <peter@inspiredpixel.net>
2011-06-18 06:12:03 +0100

    Added a task to say contact patients if their appointment is today

--- config/schedule.rb
+++ config/schedule.rb
@@ -17,4 +17,11 @@
 #   runner "AnotherModel.prune_old_records"
 # end
 
+every 1.day, :at => '7:30 am' do 
+  appointments = Appointment.all :condition => ["DATE(datetime) = DATE(?)", Time.now]
+  appointment.each do |a|
+    appointment.patient.contact("Reminder: You have an appointment today at #{a.datetime} with #{a.doctor.clinic_name}. Blah Blah...")
+  end
+end
+
 # Learn more: http://github.com/javan/whenever
