724790a5a35f8e13d12193a5fb9437c15f43d04f
Peter Hamilton <peter@inspiredpixel.net>
2011-06-18 02:15:18 +0100

    Added delay update for doctors

--- public/javascripts/clinic.js
+++ public/javascripts/clinic.js
@@ -1,9 +1,40 @@
 
 var notifications_last_polled = new Date().getTime() / 1000;
 
-setInterval(function() {
-    update_notifications();
-}, 5000); //5 seconds
+$(document).ready(function() {
+    setInterval(function() {
+        update_notifications();
+        }, 5000);
+
+        $('.handle').click(function() {
+            expand_delay_editor(this);
+        });
+        $('.handle').addClass('expand');
+    });
+
+function expand_delay_editor(handle_ref){
+    $(handle_ref).parent().animate({
+        'marginRight':0,
+        }, 500);
+
+        $(handle_ref).unbind('click');
+        $(handle_ref).click(function() {
+            contract_delay_editor(handle_ref);
+        });
+        $(handle_ref).addClass('contract').removeClass('expand');
+}
+
+function contract_delay_editor(handle_ref){
+    $(handle_ref).parent().animate({
+        'marginRight':-160,
+        }, 500);
+
+        $(handle_ref).unbind('click');
+        $(handle_ref).click(function() {
+            expand_delay_editor(handle_ref);
+        });
+        $(handle_ref).addClass('expand').removeClass('contract');
+}
 
 function update_notifications(){
     $.ajax({
@@ -14,6 +45,23 @@ function update_notifications(){
             $('#notifications').prepend(new_notifications);
         },
     });
-    
+
     notifications_last_polled = new Date().getTime() / 1000;
+}
+
+
+function update_delay(doctor_id, delay){
+    $.ajax({
+        url: "/doctors/" + doctor_id + "/update_delay/" + delay,
+        success: function(new_notifications) {
+            alert("saved");
+        },
+    });
+    
+    update_doctor_status(doctor_id, delay)
+}
+
+function update_doctor_status(doctor_id, delay){
+    
+    // Code to change the class & text of the doctor's status
 }
