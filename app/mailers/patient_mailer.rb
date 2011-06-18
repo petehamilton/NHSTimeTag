836f68b8bd887bff7ddf9eefc836d231cb42eb23
Peter Hamilton <peter@inspiredpixel.net>
2011-06-18 05:53:11 +0100

    Updated patient mailer to use better defaults

--- app/mailers/patient_mailer.rb
+++ app/mailers/patient_mailer.rb
@@ -1,11 +1,12 @@
 class PatientMailer < ActionMailer::Base
   default :from => "nhstimetag@example.com"
+  default :subject => "Message From NHS Time Track"
   
   def plain_email(patient, message)  
-      mail(:to => patient.email, :subject => "Message From NHS Time Track")  
+      mail(:to => patient.email)  
   end
   
-  def plain_email(patient, message, subject = "Message From NHS Time Track")  
+  def plain_email(patient, message)  
       mail(:to => patient.email, :subject => subject, :body = message)  
   end
 end
