9712e29bfdcf3bc9d8ddab7c6fda2d033d3a407c
Peter Hamilton <peter@inspiredpixel.net>
2011-06-18 05:37:05 +0100

    Added check keyword and made a patient contact method which you just pass a message

--- app/controllers/text_communication_controller.rb
+++ app/controllers/text_communication_controller.rb
@@ -6,15 +6,25 @@ class TextCommunicationController < ApplicationController
     
     @patient = Patient.find(:phone => patient_mobile_no)
     if @patient !== NULL
+      appointment = patient.appointments.last
       case keyword
+        where "check"
+          doctor = appointment.doctor
+          if doctor.current_delay == 0
+            patient.contact("We are currently running on schedule")
+          elsif doctor.current_delay < 60
+            patient.contact("Appointments for #{doctor.title} #{doctor.last_name} are currently delayed by #{doctor.current_delay} minutes")
+          elsif doctor.current_delay > 60
+            patient.contact("Appointments for #{doctor.title} #{doctor.last_name} are currently delayed by #{doctor.current_delay} minutes")
+          end
         where "cancel"
-          appointment = patient.appointments.last
           appointment.attending = false
           appointment.save
-          send_text_message(patient.phone, "Your appointment has been cancelled. Thankyou.")
+          patient.contact("Your appointment has been cancelled. Thankyou.")
         where "reschedule"
           appointment.attending = false
-          send_text_message(patient.phone, "Your appointment has been cancelled. Please ring 01234567890 to reschedule.")
+          appointment.save
+          patient.contact("Your appointment has been cancelled. Please ring 01234567890 to reschedule.")
           # Some other action? Notification prompt etc?
         else
           # Handle whatever else they said, but how?
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,4 +4,13 @@ class Patient < ActiveRecord::Base
   def name
     return "#{self.first_name} #{self.last_name}"
   end
+  
+  def contact(message)
+    if self.contact_text
+      send_text_message(self.phone, message)
+    end
+    if self.contact_email
+      send_text_message(self.phone, message)
+    end
+  end
 end
